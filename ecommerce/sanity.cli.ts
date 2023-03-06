dde0eb1b0a8fba79671491d099f1ca5b0bfa33cf
TheRealDammy <gdolapodamilola@gmail.com>
2023-03-06 14:43:16 +0100

    fifth commit -2

--- ecommerce/sanity.cli.ts
+++ ecommerce/sanity.cli.ts
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'xuxiqa1p',
-    dataset: 'production'
-  }
-})
-=======
 import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
@@ -16,4 +6,3 @@ export default defineCliConfig({
     dataset: 'production',
   }
 })
->>>>>>> fd0a535d4fdb8f49853193cf6fddffc8b5c8e396
