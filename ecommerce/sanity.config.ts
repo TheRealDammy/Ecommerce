cc8fb94090edb1cbec36b1d6c3727a6fc8df0b1c
TheRealDammy <gdolapodamilola@gmail.com>
2023-03-06 15:28:54 +0100

    fifth commit

--- ecommerce/sanity.config.ts
+++ ecommerce/sanity.config.ts
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'ecommerce',
-
-  projectId: 'xuxiqa1p',
-  dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
-})
-=======
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
@@ -36,4 +17,3 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
->>>>>>> fd0a535d4fdb8f49853193cf6fddffc8b5c8e396
